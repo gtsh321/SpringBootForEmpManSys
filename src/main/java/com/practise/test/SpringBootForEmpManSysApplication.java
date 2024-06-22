3344333a7d8ba7384aad1c0a3c4d54c8fa4c7236
geetesh <gtsh321@gmail.com>
2024-06-22 10:36:06 +0530

    created a project, configure the db and created packages for MVC

---
+++ src/main/java/com/practise/test/SpringBootForEmpManSysApplication.java
@@ -0,0 +1,13 @@
+package com.practise.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForEmpManSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForEmpManSysApplication.class, args);
+	}
+
+}
