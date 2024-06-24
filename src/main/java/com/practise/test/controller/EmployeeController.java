f0da7ef8ade8d266021c04e81785059f78cc142f
geetesh <gtsh321@gmail.com>
2024-06-24 19:03:59 +0530

    added the cors annotation in the controller

--- src/main/java/com/practise/test/controller/EmployeeController.java
+++ src/main/java/com/practise/test/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.practise.test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class EmployeeController {
 	
 	//get all employees rest api
     
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees()
 	{
