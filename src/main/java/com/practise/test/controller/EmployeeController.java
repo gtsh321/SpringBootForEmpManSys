807a3c2033243711346519b85d1a47bcb5f2b75d
geetesh <gtsh321@gmail.com>
2024-06-26 00:05:21 +0530

    added a new controller which gets the employee details based on primary key ID

--- src/main/java/com/practise/test/controller/EmployeeController.java
+++ src/main/java/com/practise/test/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.practise.test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practise.test.exception.ResourceNotFoundException;
 import com.practise.test.model.Employee;
 import com.practise.test.repository.EmployeeRepository;
 
@@ -30,9 +33,22 @@ public class EmployeeController {
 		
 	}
 	
+	//add employee rest api
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/createemployees")
 	public Employee createEmployee(@RequestBody Employee employee)
 	{
 		return employeeRepository.save(employee);
 	}
+	
+	//get employee by id rest api 
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id)
+	{
+		Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("employee not exist with id : "+ id));
+		return ResponseEntity.ok(employee);
+	}
+	
+	
 }
